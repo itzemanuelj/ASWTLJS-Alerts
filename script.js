f329e830cc7fb9aff07cb39f8d01380d6a290bb2
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-03 22:23:32 -0400

    added code in script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// What is the keyword that displays a message box?
+alert ("hello world!");
+// Type the character that ends every statement.
+;
+// Type the 2 characters that are missing in this statement.
+alert("You're learning JavaScript!");
+// ()
+// A __________ ends in a semicolon.
+statement
+// Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto the screen to form an alert.
+
+alert ("Hi , there sailor!");
+// Code an alert that displays the message "Thanks for everything!"
+alert ("Thanks for everything!");
+// Code an alert that displays the message "Boo!"
+alert ("Boo!");
+// The code displays a box with a message in it. Fill in the blank.
+// ______("You won!");
+alert ("You won !")
+// The code displays a box with a message in it. Fill in the blank.
+// alert("You won!____
+alert ("You Wwon!")
+// Code an alert that displays the message "Hi"
+alert ("Hi");
+// (What are the 5 punctuation characters of an alert statement, in order?
+("");
+
+
+
